fd31d1da7d2f6e8b1ae0d04173577bf70bfb7708
Haseeb Zia <haseeb_zia@epam.com>
2021-02-03 17:49:34 +0000

    compiling issues resolved further

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
 export class HomePageContainer extends React.Component {
-    render(): React.ReactNode {
-        return (
-            <div className="home">
-                <h1>Home Page</h1>
-            </div>
-
-        )
-    }
+  render(): React.ReactNode {
+    return (
+      <div className="home">
+        <h1>Home Page</h1>
+      </div>
+    );
+  }
 }
