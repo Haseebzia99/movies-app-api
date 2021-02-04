d1c04a3e072743d37f501aeb8a0284c22308a140
Haseeb Zia <haseeb_zia@epam.com>
2021-02-04 14:50:06 +0000

    webpack changes

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Banner = (): JSX.Element => {
+  return (
+    <div className="banner">
+      <div className="banner__wrapper">
+        <div className="banner__title"> FIND YOUR MOVIE </div>
+        <div className="banner__search-bar"></div>
+      </div>
+    </div>
+  );
+};
