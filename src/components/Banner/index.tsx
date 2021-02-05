341390b2bc20853eb12d38fe84d65374e84e773f
Haseeb Zia <haseeb_zia@epam.com>
2021-02-05 15:13:17 +0000

    banner and title, webpack fixed

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -4,7 +4,9 @@ export const Banner = (): JSX.Element => {
   return (
     <div className="banner">
       <div className="banner__wrapper">
-        <div className="banner__title"> FIND YOUR MOVIE </div>
+        <div className="banner__title">
+          <h1>FIND YOUR MOVIE </h1>
+        </div>
         <div className="banner__search-bar"></div>
       </div>
     </div>
